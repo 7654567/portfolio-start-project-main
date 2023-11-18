1dcca70b3a1d2dd4d76f65271a8314a1b4beab75
7654567 <khiblinsergiy@gmail.com>
2023-11-18 11:30:30 +0100

    add assets

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,151 @@ import styled from 'styled-components';
 function App() {
     return (
         <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
+            <Title>Welcome to IT-INCUBATOR1</Title>
+            Home
+            Skills
+            Works Works
+            Testimony
+            Contact
+            Hi There
+            I am Svetlana Dyablo
+            A Web Developer.
+            M y S k i l l s
+            html5
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            css3
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            React
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            typescript
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            styled components
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            WEB DESIgN
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            My Works
+            All landing page React spa
+            Social Network
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            demo Code
+            Timer
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et
+            dolore magna aliqua Ut enim
+            demo Code
+            view project
+            Testimony
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            @ivan ivanow
+            Contact
+            Send message
+            Name
+            Name
+            Name
+            I Am Available For Freelance
+            Hire me
+            © 2023 Svetlana Dyablo, All Rights Reserved.
+            Sve t l a n a
+            Hi There
+            I am
+
+            Svetlana Dyablo
+            A Web Developer.
+            M y S k i l l s
+            html5
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            css3
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            react
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            typescript
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            styled components
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            web design
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim
+            © 2023 Svetlana Dyablo, All Rights Reserved.
+            S v e t l a n a
+            My Works
+            All landing page React spa
+            view project
+            Social Network
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna
+            aliqua Ut enim. Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit.
+            demo Code
+            Timer
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna
+            aliqua Ut enim. Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit ut
+            labore et dolore magna aliqua Ut enim
+            demo Code
+            view project
+            Testimony
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua Ut
+            enim. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit.
+            @ivan ivanow
+            Contact
+            Send message
+            Name
+            Name
+            Name
+            I Am Available For
+            Freelance
+            Hire me
+            Home
+            Skills
+            Works Works
+            Testimony
+            Contact
         </div>
     );
 }
