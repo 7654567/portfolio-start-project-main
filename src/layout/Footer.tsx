87982a36e2c15d7d78ced52ccd17e5332f863885
7654567 <khiblinsergiy@gmail.com>
2023-11-27 11:57:15 +0100

    footer styles

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import styled from "styled-components";
-import sprite from "../assets/svg/sprite.svg";
+import {Icon} from "../components/Icon";
+import {colors} from "../styles/theme";
 
+const socData = [
+	{icon:"instagram", link:"telegram.org"},
+	{icon:"telegram", link:"telegram.org"},
+	{icon:"vk", link:"telegram.org"},
+	{icon:"linkedin", link:"telegram.org"}
+]
 // type FooterPropsTypes = {}
 export const Footer = () => {
 	return (
 		<SteyledFooter>
+			<h3>Svetlana</h3>
 			<ul>
-				<li>
-					<a href="#">
-						<svg>
-							<use xlinkHref={`${sprite}#telegram`}/>
-						</svg>
-					</a>
-				</li>
-				<li><a href="#"></a></li>
-				<li><a href="#"></a></li>
-				<li><a href="#"></a></li>
+				{socData.map((item) => {
+					return (
+						<li>
+							<a href={item.link}>
+								<Icon iconId={item.icon}></Icon>
+							</a>
+						</li>
+					)
+				})}
+
 			</ul>
 			<p>© 2023 Svetlana Dyablo, All Rights Reserved.</p>
 		</SteyledFooter>
@@ -30,4 +38,34 @@ const SteyledFooter = styled.div`
       font-size: 12px;
       color: rgba(255, 255, 255, 0.50);
 	}
+  h3 {
+	text-align: center;
+    font-family: "Josefin Sans", "sans-serif";
+    font-size: 22px;
+    font-weight: 700;
+    letter-spacing: 3px;
+  }
+  ul {
+	margin: 30px 0;
+	display: flex;
+	justify-content: center;
+	gap: 20px;
+	a {
+	  display: block;
+      width: 35px;
+      height: 35px;
+      border-radius: 25px;
+      background: rgba(255, 255, 255, 0.10);
+	  padding: 7px;
+      svg {
+        fill: ${colors.acsent};
+      }
+	  &:hover {
+        background: ${colors.acsent};
+		svg {
+          fill: ${colors.primary};
+		}
+	  }
+	}
+  }
 `
